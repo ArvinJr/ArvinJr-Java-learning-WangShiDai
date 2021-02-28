c79132573e6cea9432f532addf58b2f157bda9e3
arvin <arvinjunior@163.com>
2021-02-28 23:15:35 +0800

    first commit

---
+++ Day01/src/com/wangshidai/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.wangshidai;
+
+/**
+ * @author arvin
+ * @date 2021/2/28
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
