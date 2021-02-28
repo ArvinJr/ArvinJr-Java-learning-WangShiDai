cc46e048ff15519c3a8a542f92e465ee57413976
arvin <arvinjunior@163.com>
2021-02-28 23:40:07 +0800

    Day01作业

---
+++ Day01/src/com/wangshidai/HomeWork02.java
@@ -0,0 +1,13 @@
+package com.wangshidai;
+
+/**
+ * @author arvin
+ * @date 2021/2/28
+ */
+public class HomeWork02 {
+
+    public static void main(String[] args) {
+        System.out.println("我爱学java");
+    }
+
+}
